886cb97d6c2ed09e346b1fe059e532f89de3be57
unknown <prathamjagga123@gmail.com>
2023-04-24 22:16:47 +0530

    use env var

--- db/init.js
+++ db/init.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const { MONGOURI } = require("../config");
+const { MONGOURI } = require("../config"); // on local environment you can import the vars from config.js
 
 async function connectDB() {
   try {
-    await mongoose.connect(MONGOURI);
+    await mongoose.connect(process.env.MONGOURI); // for purpose of deployment I am using env var
     console.log("Connected to DB Successfully.");
   } catch (err) {
     console.log("Error Occured while connecting to DB\n", err);
